53e7eb56a8e2059e711254e8ee037c069f6afec1
jd_786 <jaydeep_shil@certree.com>
2022-05-24 08:29:11 +0530

    Add project files.

---
+++ Maximum Product Subarray/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Maximum_Product_Subarray
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] nums = new int[] { -2, 3, -4 };
+      Solution s = new Solution();
+      var result = s.MaxProduct(nums);
+      Console.WriteLine(result);
+    }
+  }
+
+  public class Solution
+  {
+    public int MaxProduct(int[] nums)
+    {
+      int result = nums[0];
+      int imax = result;
+      int imin = result;
+      for (int i = 1; i < nums.Length; i++)
+      {
+        if (nums[i] < 0)
+          (imax, imin) = Swap(imax, imin);
+
+        if (nums[i] == 0)
+        {
+          imax = 0;
+          imin = 0;
+        }
+
+        imax = Math.Max(nums[i], nums[i] * imax);
+        imin = Math.Min(nums[i], nums[i] * imin);
+
+        result = Math.Max(imax, result);
+      }
+
+      return result;
+    }
+
+    private (int, int) Swap(int imax, int imin)
+    {
+      int temp = imax;
+      imax = imin;
+      imin = temp;
+
+      return (imax, imin);
+    }
+  }
+}
